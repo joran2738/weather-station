2980c2c52fb04d12a62cf4fd433b96486fb158a4
joran2738 <joranvannieuwenhoven@gmail.com>
2022-04-13 18:04:47 +0200

    lib change and src code setup

---
+++ src/binnenmodule.ino
@@ -0,0 +1,117 @@
+///LORA///////////////////////////////
+#include <SPI.h>              // include libraries
+#include <LoRa.h>
+
+const int csPin = 7;          // LoRa radio chip select
+const int resetPin = 6;       // LoRa radio reset
+const int irqPin = 1;         // change for your board; must be a hardware interrupt pin
+
+String outgoing;              // outgoing message
+
+byte msgCount = 0;            // count of outgoing messages
+byte localAddress = 0xFF;     // address of this device
+byte destination = 0xBB;      // destination to send to
+long lastSendTime = 0;        // last send time
+int interval = 2000;          // interval between sends
+
+///EEPROM////////////////////////////
+#include <EEPROM.h>
+int var1 = 0
+/...
+
+///weather///////////////////////////
+#include <BME280.h>
+#include <BME280I2C.h>
+#include <BME280I2C_BRZO.h>
+#include <BME280Spi.h>
+#include <BME280SpiSw.h>
+#include <EnvironmentCalculations.h>
+
+int regen=0;
+int LDR=0;
+/...
+
+///LCD//////////////////////////////
+//initialise libraries
+
+#include <Adafruit_GFX.h>    // Core graphics library
+#include <Adafruit_ST7735.h> // Hardware-specific library
+#include <SPI.h>
+
+//initialise names/ports
+
+// For the breakout, you can use any 2 or 3 pins
+// These pins will also work for the 1.8" TFT shield
+#define TFT_CS     10
+#define TFT_RST    9  // you can also connect this to the Arduino reset // in which case, set this #define pin to 0!
+#define TFT_DC     8
+
+Adafruit_ST7735 tft = Adafruit_ST7735(TFT_CS,  TFT_DC, TFT_RST);
+
+// color definitions
+const uint16_t  Display_Color_Black        = 0x0000;
+const uint16_t  Display_Color_Blue         = 0x001F;
+const uint16_t  Display_Color_Red          = 0xF800;
+const uint16_t  Display_Color_Green        = 0x07E0;
+const uint16_t  Display_Color_Cyan         = 0x07FF;
+const uint16_t  Display_Color_Magenta      = 0xF81F;
+const uint16_t  Display_Color_Yellow       = 0xFFE0;
+const uint16_t  Display_Color_White        = 0xFFFF;
+
+// The colors we actually want to use
+uint16_t        Display_Text_Color         = Display_Color_Black;
+uint16_t        Display_Backround_Color    = Display_Color_Blue;
+
+// assume the display is off until configured in setup()
+bool            isDisplayVisible        = false;
+
+// declare size of working string buffers. Basic strlen("d hh:mm:ss") = 10
+const size_t    MaxString               = 16;
+
+// the string being displayed on the SSD1331 (initially empty)
+char oldTimeString[MaxString]           = { 0 };
+
+// the interrupt service routine affects this
+volatile bool   isButtonPressed         = false;
+
+void setup() {
+  // put your setup code here, to run once:
+  Serial.begin(SERIAL_BAUD);
+
+  while(!Serial) {} // Wait
+///LORA///////////////////////////////
+  Serial.println("LoRa Duplex");
+  // override the default CS, reset, and IRQ pins (optional)
+  LoRa.setPins(csPin, resetPin, irqPin);// set CS, reset, IRQ pin
+
+  if (!LoRa.begin(433E6)) {             // initialize ratio at 915 MHz
+    Serial.println("LoRa init failed. Check your connections.");
+    while (true);                       // if failed, do nothing
+  }
+
+  Serial.println("LoRa init succeeded.");
+///LCD///////////////////////////////
+  Serial.print("Hello! ST7735 TFT Test");
+
+  delay(250);
+
+  // Use this initializer if you're using a 1.8" TFT
+  tft.initR(INITR_BLACKTAB);   // initialize a ST7735S chip, black tab
+
+// initialise the display
+    tft.setFont();
+    tft.fillScreen(Display_Backround_Color);
+    tft.setTextColor(Display_Text_Color);
+    tft.setTextSize(1);
+
+    // the display is now on
+    isDisplayVisible = true;
+
+  Serial.println("Initialized");
+  
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
---
+++ src/buitenmodule.ino
@@ -0,0 +1,130 @@
+﻿
+///LORA///////////////////////////////
+#include <SPI.h>              // include libraries
+#include <LoRa.h>
+
+const int csPin = 7;          // LoRa radio chip select
+const int resetPin = 6;       // LoRa radio reset
+const int irqPin = 1;         // change for your board; must be a hardware interrupt pin
+
+String outgoing;              // outgoing message
+
+byte msgCount = 0;            // count of outgoing messages
+byte localAddress = 0xBB;     // address of this device
+byte destination = 0xFF;      // destination to send to
+long lastSendTime = 0;        // last send time
+int interval = 2000;          // interval between sends
+
+///BME280////////////////////////////
+#include <BME280I2C.h>
+#include <Wire.h>             // Needed for legacy versions of Arduino.
+
+#define SERIAL_BAUD 9600
+
+/* Recommended Modes -
+   Based on Bosch BME280I2C environmental sensor data sheet.
+
+Weather Monitoring :
+   forced mode, 1 sample/minute
+   pressure ×1, temperature ×1, humidity ×1, filter off
+   Current Consumption =  0.16 μA
+   RMS Noise = 3.3 Pa/30 cm, 0.07 %RH
+   Data Output Rate 1/60 Hz
+
+Humidity Sensing :
+   forced mode, 1 sample/second
+   pressure ×0, temperature ×1, humidity ×1, filter off
+   Current Consumption = 2.9 μA
+   RMS Noise = 0.07 %RH
+   Data Output Rate =  1 Hz
+
+Indoor Navigation :
+   normal mode, standby time = 0.5ms
+   pressure ×16, temperature ×2, humidity ×1, filter = x16
+   Current Consumption = 633 μA
+   RMS Noise = 0.2 Pa/1.7 cm
+   Data Output Rate = 25Hz
+   Filter Bandwidth = 0.53 Hz
+   Response Time (75%) = 0.9 s
+
+
+Gaming :
+   normal mode, standby time = 0.5ms
+   pressure ×4, temperature ×1, humidity ×0, filter = x16
+   Current Consumption = 581 μA
+   RMS Noise = 0.3 Pa/2.5 cm
+   Data Output Rate = 83 Hz
+   Filter Bandwidth = 1.75 Hz
+   Response Time (75%) = 0.3 s
+
+*/
+
+BME280I2C::Settings settings(
+   BME280::OSR_X1,
+   BME280::OSR_X1,
+   BME280::OSR_X1,
+   BME280::Mode_Forced,
+   BME280::StandbyTime_1000ms,
+   BME280::Filter_Off,
+   BME280::SpiEnable_False,
+   BME280I2C::I2CAddr_0x76 // I2C address. I2C specific.
+);
+
+BME280I2C bme(settings);
+
+///regensensor////////////////////////
+int regensensor = A0;    // select the input pin for the rainsensor
+int regen = 0;  // variable to store the value coming from the sensor
+
+///LDR////////////////////////////////
+int LDRsensor = A1;    // select the input pin for the rainsensor
+int LDR = 0;  // variable to store the value coming from the sensor
+
+//////////////////////////////////////////////////////////////////
+void setup()
+{
+  Serial.begin(SERIAL_BAUD);
+
+  while(!Serial) {} // Wait
+  
+///LORA///////////////////////////////
+  Serial.println("LoRa Duplex");
+  // override the default CS, reset, and IRQ pins (optional)
+  LoRa.setPins(csPin, resetPin, irqPin);// set CS, reset, IRQ pin
+
+  if (!LoRa.begin(433E6)) {             // initialize ratio at 915 MHz
+    Serial.println("LoRa init failed. Check your connections.");
+    while (true);                       // if failed, do nothing
+  }
+
+  Serial.println("LoRa init succeeded.");
+
+///BME280////////////////////////////
+  Wire.begin();
+  while(!bme.begin())
+  {
+    Serial.println("Could not find BME280I2C sensor!");
+    delay(1000);
+  }
+
+  switch(bme.chipModel())
+  {
+     case BME280::ChipModel_BME280:
+       Serial.println("Found BME280 sensor! Success.");
+       break;
+     case BME280::ChipModel_BMP280:
+       Serial.println("Found BMP280 sensor! No Humidity available.");
+       break;
+     default:
+       Serial.println("Found UNKNOWN sensor! Error!");
+  }
+
+   // Change some settings before using.
+   settings.tempOSR = BME280::OSR_X4;
+
+   bme.setSettings(settings);
+}
+void loop(){
+  
+  
+  }

42a9c6e76d8872088043c55912332062ff23a9ab
joran2738 <101818067+joran2738@users.noreply.github.com>
2022-05-30 14:47:07 +0200

    Update instelingen_weerstation.ino

--- src/instelingen_weerstation.ino
+++ src/instelingen_weerstation.ino
@@ -2,91 +2,95 @@
 #include  <Adafruit_GFX.h>
 #include <Wire.h>
 
-const int menu = 2;
-const int omhoog = 3;
+const int menu = 3;
+const int omhoog = 2;
 const int omlaag = 4;
-int ingedrukt = 0;
-int terug = 0;
-byte neerslag = true;
-byte temp = false;
-byte luchtvochtigheid = false;
-byte druk = false;
-byte licht = false;
-int scroll = 6;
-byte inst = false;      
+byte ingedrukt = 0;
+byte terug = 0;
+bool neerslag = true;
+bool temp = false;
+bool luchtvochtigheid = false;
+bool druk = false;
+int scroll = 4;
+bool inst = false;      
 
 void setup() {
-  pinMode(menu, INPUT);
-  pinMode(omhoog, INPUT);
-  pinMode(omlaag, INPUT);
+  pinMode(menu, INPUT_PULLUP);
+  pinMode(omhoog, INPUT_PULLUP);
+  pinMode(omlaag, INPUT_PULLUP);
+  Serial.begin(9600); while (!Serial); Serial.println();
 }
 
 void loop() {
-instellingen();
+  instellingen();
+  Serial.println("out of settings");
+  delay(100);
 }
 
 void instellingen(){
+  scroll = 4;
   ingedrukt = digitalRead(menu);
   inst = menu;
-  if (ingedrukt == HIGH)
-  { //start instellingen
+  
+  if (ingedrukt == LOW){ //start instellingen
+    
+    Serial.println("entered settings");
     terug = 0;
-    while(terug == 0)
-    {
-      if(digitalRead(omhoog) == 1)
-      {//zet de select zone 1 omhoog
-        if(scroll > 0){
+    
+    while(terug == 0){
+      if(digitalRead(omhoog) == 0){//zet de select zone 1 omhoog
+        if(scroll > 1){
           scroll = scroll - 1;
+          Serial.println(scroll);
           delay(500);//om de knoppen te debouncen
         }
       }
-      if(digitalRead(omlaag) == 1)
-        {//zet de select zone 1 omlaag
-        if(scroll < 7){
-          scroll = scroll + 1;
-          delay(500);//om de knoppen te debouncen
-        }
-        }
+      
+      if(digitalRead(omlaag) == 0){//zet de select zone 1 omlaag
+          if(scroll < 4){
+            scroll = scroll + 1;
+            Serial.println(scroll);
+            delay(500);//om de knoppen te debouncen
+          }
+      }
+      
+      
       switch(scroll){
-        case 6: //neerslag mm/m² - %
-        if(digitalRead(menu) == 1)
-        {//selecteer de optie
-          neerslag = !(neerslag); 
-          delay(500);
-        }
-        break;
-        case 5: //temperatuur °C - °F
-        if(digitalRead(menu) == 1)
-        {//selecteer de optie
+
+        case 4: //temperatuur °C - °F
+        if(digitalRead(menu) == 0){//selecteer de optie
           temp = !(temp); 
+          Serial.println("temp"+String(temp));
           delay(500);
         }
         break;
-        case 4: //luchtvochtigheid % - ppm
-        if(digitalRead(menu) == 1)
-        {//selecteer de optie
+          
+        case 3: //luchtvochtigheid % - ppm
+        if(digitalRead(menu) == 0){//selecteer de optie
           luchtvochtigheid = !(luchtvochtigheid); 
+          Serial.println("luchtvochtigheid"+String(luchtvochtigheid));
           delay(500);
         }
         break;
-        case 3: //druk bar - P
-        if(digitalRead(menu) == 1)
-        {//selecteer de optie
+          
+        case 2: //druk bar - P
+        if(digitalRead(menu) == 0){//selecteer de optie
           druk = !(druk); 
+          Serial.println("druk"+String(druk));
           delay(500);
         }
         break;
-        case 2: //lichtintensiteit lux - % 
-        if(digitalRead(menu) == 1)
-        {//selecteer de optie
-          licht = !(licht); 
-          delay(500);
-        }
-        break;
+
         case 1: //terug
-        terug = 1;
+        if(digitalRead(menu) == 0){//selecteer de optie
+          terug = 1; 
+          Serial.println("going back");
+          delay(500);
+        }
         break;
       }
+      
+
     }
   }
 }
